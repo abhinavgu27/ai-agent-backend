a2809385bc4afe431b9601d0c58ec925f160f852
Abhinavgu27 <abhinavsinghal29oct@gmail.com>
2026-05-13 20:40:03 +0530

    Update Render URL

--- private-ui/src/App.jsx
+++ private-ui/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
     try {
       // Using 127.0.0.1 to avoid DNS resolution issues on Windows
-      const response = await fetch('https://ai-agent-backend-cmda.onrender.com', {
+      const response = await fetch('https://ai-agent-backend-nu.vercel.app/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input, history: chatLog })
