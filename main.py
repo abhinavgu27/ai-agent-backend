2870575a402d85a1ad479890da1e9f73c391908b
Abhinavgu27 <abhinavsinghal29oct@gmail.com>
2026-05-13 23:27:21 +0530

    Fix: Applied pysqlite3 shim for ChromaDB compatibility

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
